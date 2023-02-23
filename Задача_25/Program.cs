76ea3e61e8904ea3c86d339c26613fd2cdbe28f0
Milena2101 <filimonova.milena2015@yandex.ru>
2023-02-23 14:35:29 +0300

    ...

---
+++ Задача_25/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите метод, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+*/
+Console.Clear();
+Console.Write("Number А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int pover = A;
+
+for ( int i =1; i < B; i++)
+{
+    pover = pover * A;
+}
+Console.WriteLine("А в тепени В = " + pover);
