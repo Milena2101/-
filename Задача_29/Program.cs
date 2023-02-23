a2668df396d66af30453514c72e5b58d7746c486
Milena2101 <filimonova.milena2015@yandex.ru>
2023-02-23 14:47:35 +0300

    ...

---
+++ Задача_29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+Console.Clear();
+
+int array = ReadLine("Длина массива: ");
+Console.Write("[");
+
+int[] random = new int[array];
+for (int i = 0; i < random.Length; i++)
+{
+    random[i] = new Random().Next(1, 9);
+    Console.Write(random[i] + " ");
+}
+int ReadLine(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("]");
