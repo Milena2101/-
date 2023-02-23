f2b32be4800d4571e0677fe7c1ed127a567ba9e0
Milena2101 <filimonova.milena2015@yandex.ru>
2023-02-23 14:37:39 +0300

    ...

---
+++ Задача_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Clear();
+Console.Write(" Number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int Sum_of_digits = 0;
+
+while (i > 0)
+{
+    int number = i % 10;
+    i = i / 10;
+    Sum_of_digits = Sum_of_digits + number;
+}
+Console.WriteLine("The sum is equal to = " + Sum_of_digits );
